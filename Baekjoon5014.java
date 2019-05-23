7d9ad884b8bc3906ff45b4d13c4582d53c107b4c
junwoochoi <cjw4690@gmail.com>
2019-05-23 16:35:18 +0900

    백준 5014번 추가

---
+++ Baekjoon5014.java
@@ -0,0 +1,74 @@
+package Algorithms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/5014
+ * 백준 5014번 스타트링크
+ */
+public class Baekjoon5014 {
+    private static final String USE_STAIRS = "use the stairs";
+    private static int[] building;
+    private static int[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int F = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int G = Integer.parseInt(st.nextToken());
+        int U = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        building = new int[F + 1];
+        visited = new int[F + 1];
+        Arrays.fill(visited, Integer.MAX_VALUE);
+
+
+        solution(F, S, G, U, D);
+
+    }
+
+    private static void solution(int maxFloor, int start, int company, int up, int down) {
+        Queue<Integer> queue = new LinkedList<>();
+
+
+        visited[start] = 0;
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int curentFloor = queue.poll();
+
+            int upFloor = curentFloor+up;
+            int downFloor = curentFloor-down;
+
+            if(upFloor<=maxFloor){
+                if(visited[upFloor] > visited[curentFloor]+1){
+                    visited[upFloor] = visited[curentFloor]+1;
+                    queue.add(upFloor);
+                }
+            }
+
+            if(downFloor>0){
+                if(visited[downFloor]>visited[curentFloor]+1){
+                    visited[downFloor] = visited[curentFloor]+1;
+                    queue.add(downFloor);
+                }
+            }
+        }
+
+        if(visited[company] == Integer.MAX_VALUE){
+            System.out.println(USE_STAIRS);
+            return;
+        }
+
+        System.out.println(visited[company]);
+    }
+}
